adadfa6fcf6af14a6d9f43a5e01cc57bfe363c5f
Juan Medina <jamedina@gmail.com>
2020-02-07 11:15:14 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ if (bintrayUsername != null && bintrayApiKey != null) {
         key = bintrayApiKey
 
         pkg.apply {
-            repo = "juan-medina"
+            repo = rootProject.name
             name = rootProject.name
 
             websiteUrl = "https://github.com/juan-medina/regexp-dsl/"
